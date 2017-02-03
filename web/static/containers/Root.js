2deabf24981250f250c87bfda98c2add42cc6dd9
Kien Nguyen Trung <trungkien2288@gmail.com>
2017-02-03 17:48:12 +0700

    Add GA

--- web/static/containers/Root.js
+++ web/static/containers/Root.js
@@ -2,8 +2,17 @@ import React, { PropTypes } from 'react';
 import { Router } from 'react-router';
 import { ApolloProvider } from 'react-apollo';
 
+import ReactGA from 'react-ga';
+
 import routes from '../routes';
 import DevTools from './DevTools';
+
+ReactGA.initialize('UA-16274581-12');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
 
 const Root = ({ store, history, client }) => (
   <ApolloProvider store={store} client={client}>
@@ -11,6 +20,7 @@ const Root = ({ store, history, client }) => (
       <Router
         history={history}
         routes={routes}
+        onUpdate={logPageView}
       />
       <DevTools />
     </div>
